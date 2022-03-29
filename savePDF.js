31534fc00e70910610e5840466b624293d498763
ngocsnake <ngocsnake@gmail.com>
2022-03-29 09:54:15 +0700

    version 2

--- savePDF.js
+++ savePDF.js
@@ -1,59 +1,84 @@
+import niceBytes from './module.js';
+
 const { degrees, PDFDocument, rgb, StandardFonts } = PDFLib;
-let arrayURL = []
-let outputName = ''
 
-async function modifyPdf(arrayURL) {
-    try {
-        const bfinal = await fetch(arrayURL[0]).then(res => res.arrayBuffer())
-        const finalPDF = await PDFDocument.load(bfinal);
+let arrayURL = [];
 
-        for (let i = 1; i < arrayURL.length; i++) {
-            const existingPdfBytes = await fetch(arrayURL[i]).then(res => res.arrayBuffer())
-            const fileADD = await PDFDocument.load(existingPdfBytes);
-
-            const pagesArray = await finalPDF.copyPages(fileADD, fileADD.getPageIndices());
-
-            for (let i = 1; i < pagesArray.length; i++) {
-                finalPDF.addPage(pagesArray[i]);
-            }
-        }
-
-        const pdfBytes = await finalPDF.save()
-        download(pdfBytes, outputName, "application/pdf");
-    }
-    catch (e) {
-        alert("something went wrong        (ㄒoㄒ)")
-    }
-}
+const current = document.querySelector('#current');
 
 const input = document.querySelector('#files');
-const bia = document.querySelector('#filesbia');
-
-
-bia.addEventListener('change', () => {
-    const ten = bia.files[0].name;
-    document.querySelector('#tenFileBia').textContent = ten;
-    outputName = ten;
-})
-
 
 input.addEventListener('change', () => {
     arrayURL = [];
 
-    document.querySelector('#countFiles').textContent = `Đã chọn ${input.files.length} File.`;
+    let totalSize = 0;
 
-    arrayURL.push(
-        URL.createObjectURL(bia.files[0])
-    )
     Array.from(input.files).forEach(file => {
+
+        totalSize += file.size;
+
         arrayURL.push(
-            URL.createObjectURL(file)
+            {
+                name: file.name.replace('.pdf', ''),
+                url: URL.createObjectURL(file)
+            }
         )
     })
+    document.querySelector('#countFiles').textContent = `Đã chọn ${input.files.length} File. (${niceBytes(totalSize)})`;
 })
 
-
 const ghepFile = () => {
-    modifyPdf(arrayURL);
+    const isConfirm = confirm('Chắc chắn ghép?');
+
+    if (isConfirm) {
+        ghepFilePDF(arrayURL)
+    }
 }
 
+async function ghepFilePDF(arrayURL) {
+
+    const arrayBia = arrayURL.filter(e => e.name.length === 25);
+
+    arrayBia.forEach(bia => {
+        bia.fileghep = arrayURL.filter(
+            e => e.name !== bia.name && e.name.startsWith(bia.name)
+        );
+    })
+
+    for (let i = 0; i < arrayBia.length; i++) {
+
+        const file = arrayBia[i];
+
+        const biaURL = file.url;
+        const fileName = file.name + '.pdf'
+
+        current.textContent = 'Đang ghép: ' + fileName + '...';
+
+        const fileGhep = file.fileghep;
+
+        try {
+            const bfinal = await fetch(biaURL).then(res => res.arrayBuffer())
+            URL.revokeObjectURL(biaURL)
+            const finalPDF = await PDFDocument.load(bfinal);
+
+            for (let i = 0; i < fileGhep.length; i++) {
+                const existingPdfBytes = await fetch(fileGhep[i].url).then(res => res.arrayBuffer())
+                URL.revokeObjectURL(fileGhep[i].url)
+                const fileADD = await PDFDocument.load(existingPdfBytes);
+
+                const pagesArray = await finalPDF.copyPages(fileADD, fileADD.getPageIndices());
+
+                for (let i = 1; i < pagesArray.length; i++) {
+                    finalPDF.addPage(pagesArray[i]);
+                }
+            }
+            const pdfBytes = await finalPDF.save()
+            download(pdfBytes, fileName, "application/pdf");
+        }
+        catch (e) {
+            console.log(e);
+            alert("something went wrong        (ㄒoㄒ)")
+        }
+        current.textContent = `\\(^o^)/`;
+    }
+}
